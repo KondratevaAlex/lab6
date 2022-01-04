60a697532a3915378006d1eee7747dee4cfe8ab6
Sasha <aleksagerhardt@gmail.com>
2022-01-04 19:29:42 +0300

    Work with massive

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace lab6
 	{
 		static void Main(string[] args)
 		{
-			double ansper, anssur, dif;
+			double[] ansper = new double[2];
+			double[] anssur = new double[2];
+			double difper, difsur;
 			int choice;
 			do
 			{
@@ -16,84 +18,132 @@ namespace lab6
 				{
 					case 1:
 						{
-							Triangle arr = new Triangle();
-							ObjectInfo obj = new ObjectInfo();
-
-							Console.WriteLine("\nВведите данные  треугольника:\n");
-							arr.settri(arr);
-							obj.setInfo(obj);
-							arr.printtri(arr);
-							obj.printInfo(obj);
-							ansper = arr.triperimeter(arr);
-							anssur = arr.trisurf(arr);
-
-							dif = anssur / ansper;
-							Console.Write("\n\nОтношение объема треугольника к его периметру: ");
-							Console.Write(dif);
+							Triangle [] arr = new Triangle[2];
+							arr[0] = new Triangle();
+							arr[1] = new Triangle();
+							ObjectInfo [] obj = new ObjectInfo[2];
+							obj[0] = new ObjectInfo();
+							obj[1] = new ObjectInfo();
+							for (int i = 0; i < 2; i++)
+							{
+								Console.WriteLine("\nВведите данные  треугольника:\n");
+								arr[i].settri(arr[i]);
+								obj[i].setInfo(obj[i]);
+								arr[i].printtri(arr[i]);
+								obj[i].printInfo(obj[i]);
+								ansper[i] = arr[i].triperimeter(arr[i]);
+								anssur[i] = arr[i].trisurf(arr[i]);
+							}
+							difper = ansper[0] / ansper[1];
+							difsur = anssur[0] / anssur[1];
+							Console.Write("\n\nОтношение периметров треугольников друг к другу: ");
+							Console.Write(difper);
+							Console.Write("\nОтношение площадей треугольников друг к другу: ");
+							Console.Write(difsur);
 							break;
 						}
 					case 2:
 						{
-							Rectangle arr = new Rectangle();
-							ObjectInfo obj = new ObjectInfo();
-							Console.WriteLine("\nВведите данные прямоугольника:\n");
-							arr.setrect(arr);
-							obj.setInfo(obj);
-							arr.printrect(arr);
-							obj.printInfo(obj);
-							ansper = arr.rectperimeter(arr);
-							anssur = arr.rectsurf(arr);
-							dif = anssur / ansper;
-							Console.WriteLine("\n\nОтношение объема прямоугольника к его периметру: ");
-							Console.Write(dif);
+							Rectangle [] arr = new Rectangle[2];
+							arr[0] = new Rectangle();
+							arr[1] = new Rectangle();
+							ObjectInfo [] obj = new ObjectInfo[2];
+							obj[0] = new ObjectInfo();
+							obj[1] = new ObjectInfo();
+							for (int i = 0; i < 2; i++)
+							{
+								Console.WriteLine("\nВведите данные прямоугольника:\n");
+								arr[i].setrect(arr[i]);
+								obj[i].setInfo(obj[i]);
+								arr[i].printrect(arr[i]);
+								obj[i].printInfo(obj[i]);
+								ansper[i] = arr[i].rectperimeter(arr[i]);
+								anssur[i] = arr[i].rectsurf(arr[i]);
+							}
+							difper = ansper[0] / ansper[1];
+							difsur = anssur[0] / anssur[1];
+							Console.Write("\n\nОтношение периметров прямоугольников друг к другу: ");
+							Console.Write(difper);
+							Console.Write("\nОтношение площадей прямоугольников друг к другу: ");
+							Console.Write(difsur);
 							break;
 						}
 					case 3:
 						{
-							Parallelogram arr = new Parallelogram();
-							ObjectInfo obj = new ObjectInfo();
-							Console.WriteLine("\nВведите данные %d параллелограмма:\n");
-							arr.setpar(arr);
-							obj.setInfo(obj);
-							arr.printpar(arr);
-							obj.printInfo(obj);
-							ansper = arr.parperimeter(arr);
-							anssur = arr.parsurf(arr);
-							dif = anssur / ansper;
-							Console.WriteLine("\n\nОтношение объема параллелограмма к его периметру: ");
-							Console.Write(dif);
+							Parallelogram [] arr = new Parallelogram[2];
+							arr[0] = new Parallelogram();
+							arr[1] = new Parallelogram();
+							ObjectInfo [] obj = new ObjectInfo[2];
+							obj[0] = new ObjectInfo();
+							obj[1] = new ObjectInfo();
+							for (int i = 0; i < 2; i++)
+							{
+								Console.WriteLine("\nВведите данные %d параллелограмма:\n");
+								arr[i].setpar(arr[i]);
+								obj[i].setInfo(obj[i]);
+								arr[i].printpar(arr[i]);
+								obj[i].printInfo(obj[i]);
+								ansper[i] = arr[i].parperimeter(arr[i]);
+								anssur[i] = arr[i].parsurf(arr[i]);
+							}
+							difper = ansper[0] / ansper[1];
+							difsur = anssur[0] / anssur[1];
+							Console.Write("\n\nОтношение периметров параллелограммов друг к другу: ");
+							Console.Write(difper);
+							Console.Write("\nОтношение площадей параллелограммов друг к другу: ");
+							Console.Write(difsur);
 							break;
 						}
 					case 4:
 						{
-							Trapezoid arr = new Trapezoid();
-							ObjectInfo obj = new ObjectInfo();
-							Console.WriteLine("\nВведите данные трапеции:\n");
-							arr.settrap(arr);
-							obj.setInfo(obj);
-							arr.printtrap(arr);
-							obj.printInfo(obj);
-							ansper = arr.trapperimeter(arr);
-							anssur = arr.trapsurf(arr);
-							dif = anssur / ansper;
-							Console.WriteLine("\n\nОтношение объема трапеции к его периметру: ");
-							Console.Write(dif);
+							Trapezoid [] arr = new Trapezoid[2];
+							arr[0] = new Trapezoid();
+							arr[1] = new Trapezoid();
+							ObjectInfo [] obj = new ObjectInfo[2];
+							obj[0] = new ObjectInfo();
+							obj[1] = new ObjectInfo();
+							for (int i = 0; i < 2; i++)
+							{
+								Console.WriteLine("\nВведите данные трапеции:\n");
+								arr[i].settrap(arr[i]);
+								obj[i].setInfo(obj[i]);
+								arr[i].printtrap(arr[i]);
+								obj[i].printInfo(obj[i]);
+								ansper[i] = arr[i].trapperimeter(arr[i]);
+								anssur[i] = arr[i].trapsurf(arr[i]);
+							}
+							difper = ansper[0] / ansper[1];
+							difsur = anssur[0] / anssur[1];
+							Console.Write("\n\nОтношение периметров трапеций друг к другу: ");
+							Console.Write(difper);
+							Console.Write("\nОтношение площадей трапеций друг к другу: ");
+							Console.Write(difsur);
 							break;
 						}
 					case 5:
 						{
-							Circle arr = new Circle();
-							ObjectInfo obj = new ObjectInfo();
-							Console.WriteLine("\nВведите данные %d круга:\n");
-							arr.setcirc(arr);
-							obj.setInfo(obj);
-							arr.printcirc(arr);
-							obj.printInfo(obj);
-							ansper = arr.circperimeter(arr);
-							anssur = arr.circsurf(arr);
-							dif = anssur / ansper;
-							Console.WriteLine("\n\nОтношение объема круга к его периметру: ");
-							Console.Write(dif);
+							Circle [] arr = new Circle[2];
+							arr[0] = new Circle();
+							arr[1] = new Circle();
+							ObjectInfo [] obj = new ObjectInfo[2];
+							obj[0] = new ObjectInfo();
+							obj[1] = new ObjectInfo();
+							for (int i = 0; i < 2; i++)
+							{
+								Console.WriteLine("\nВведите данные %d круга:\n");
+								arr[i].setcirc(arr[i]);
+								obj[i].setInfo(obj[i]);
+								arr[i].printcirc(arr[i]);
+								obj[i].printInfo(obj[i]);
+								ansper[i] = arr[i].circperimeter(arr[i]);
+								anssur[i] = arr[i].circsurf(arr[i]);
+							}
+							difper = ansper[0] / ansper[1];
+							difsur = anssur[0] / anssur[1];
+							Console.Write("\n\nОтношение периметров кругов друг к другу: ");
+							Console.Write(difper);
+							Console.Write("\nОтношение площадей кругов друг к другу: ");
+							Console.Write(difsur);
 							break;
 						}
 				}
